23c2fee722933bfd1a4c21e971c3e9d832116568
Rounak <rounakkumar040@gmail.com>
2023-05-30 13:10:42 +0530

    refactoring the code

--- script.js
+++ script.js
@@ -13,17 +13,25 @@ let secretNum = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 let highscore = 0;
 
+const displayMsg = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
   //
   if (!guess) {
-    document.querySelector('.message').textContent = '⛔ No Number!';
+    // document.querySelector('.message').textContent = '⛔ No Number!';
+
+    displayMsg('⛔ No Number!');
 
     //When player wins
   } else if (guess === secretNum) {
-    document.querySelector('.message').textContent = '🎉 Correct Number!!!';
+    // document.querySelector('.message').textContent = '🎉 Correct Number!!!';
+
+    displayMsg('🎉 Correct Number!!!');
     document.querySelector('.number').textContent = secretNum;
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
@@ -33,35 +41,54 @@ document.querySelector('.check').addEventListener('click', function () {
     }
 
     //If guess is too high
-  } else if (guess > secretNum) {
+  } else if (guess !== secretNum) {
     if (score > 1) {
-      document.querySelector('.message').textContent = '📈 Too high!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = '🥴 You Lost the game!!';
-      document.querySelector('.score').textContent = 0;
-      document.querySelector('body').style.backgroundColor = 'red';
-    }
+      //   document.querySelector('.message').textContent =
+      //     guess > secretNum ? '📈 Too high! ' : '📉 Too low! ';
 
-    //when plauer guess is too low
-  } else if (guess < secretNum) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = '📉 Too low! ';
+      displayMsg(guess > secretNum ? '📈 Too high! ' : '📉 Too low! ');
       score--;
       document.querySelector('.score').textContent = score;
     } else {
-      document.querySelector('.message').textContent = '🥴 You Lost the game!!';
+      //   document.querySelector('.message').textContent = '🥴 You Lost the game!!';
+
+      displayMsg('🥴 You Lost the game!!');
+
       document.querySelector('.score').textContent = 0;
       document.querySelector('body').style.backgroundColor = 'red';
     }
   }
+  //   } else if (guess > secretNum) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📈 Too high!';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '🥴 You Lost the game!!';
+  //       document.querySelector('.score').textContent = 0;
+  //       document.querySelector('body').style.backgroundColor = 'red';
+  //     }
+
+  //     //when plauer guess is too low
+  //   } else if (guess < secretNum) {
+  //     if (score > 1) {
+  //       document.querySelector('.message').textContent = '📉 Too low! ';
+  //       score--;
+  //       document.querySelector('.score').textContent = score;
+  //     } else {
+  //       document.querySelector('.message').textContent = '🥴 You Lost the game!!';
+  //       document.querySelector('.score').textContent = 0;
+  //       document.querySelector('body').style.backgroundColor = 'red';
+  //     }
+  //   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   secretNum = Math.trunc(Math.random() * 20) + 1;
-  document.querySelector('.message').textContent = 'Start guessing...';
+  //   document.querySelector('.message').textContent = 'Start guessing...';
+
+  displayMsg('Start guessing...');
   document.querySelector('.number').textContent = '?';
   document.querySelector('.guess').value = '';
   document.querySelector('.score').textContent = score;
